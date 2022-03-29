d0677f60050c5a7b76c5036c979a779c336619fe
souravjha20 <66384964+souravjha20@users.noreply.github.com>
2022-03-29 18:30:05 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,5 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java,welcome to jenkins git session");  
+     System.out.println("this is tuesday with 29th march 2022");     
     }  
 }  
